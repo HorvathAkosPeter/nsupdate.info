612f7c5f96de707288660b0b6aff83ef93fc7fa9
Peter Horvath <horvath.akos.peter@gmail.com>
2026-05-24 16:45:47 +0200

    multistage builds started

--- scripts/docker/test.sh
+++ scripts/docker/test.sh
@@ -3,10 +3,5 @@
 set -euxo pipefail
 
 cd /tmp && named -g -u named -c /etc/bind/named.conf.local &
-
-cd /app
-pip install -e .
-pip install -r requirements.d/dev.txt
-
 pylint src/nsupdate
 pytest src/nsupdate
